7e16958182717ae77982bd1e284ec29e4dd67a7f
Elang Aditya Rahman <elangadityar@gmail.com>
2025-03-21 15:12:22 +0700

    prefix expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -6,6 +6,7 @@ import (
 )
 
 var (
+	NULL  = &object.Null{}
 	TRUE  = &object.Boolean{Value: true}
 	FALSE = &object.Boolean{Value: false}
 )
@@ -18,8 +19,11 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
-  case *ast.Boolean:
-    return nativeBoolToBooleanObject(node.Value)
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -36,8 +40,41 @@ func evalStatements(stmts []ast.Statement) object.Object {
 }
 
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
-  if input {
-    return TRUE
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+  case "-":
+    return evalMinusPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+  if right.Type() != object.INTEGER_OBJ {
+    return NULL
   }
-  return FALSE
+
+  value := right.(*object.Integer).Value
+  return &object.Integer{Value: -value}
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -7,13 +7,23 @@ import (
 	"testing"
 )
 
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
 func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected int64
 	}{
 		{"5", 5},
+		{"-5", -5},
 		{"10", 10},
+		{"-10", -10},
 	}
 
 	for _, tt := range tests {
@@ -67,10 +77,21 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 	return true
 }
 
-func testEval(input string) object.Object {
-	l := lexer.New(input)
-	p := parser.New(l)
-	program := p.ParseProgram()
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
 
-	return Eval(program)
+  for _, tt := range tests {
+    evaluated := testEval(tt.input)
+    testBooleanObject(t, evaluated, tt.expected)
+  }
 }
