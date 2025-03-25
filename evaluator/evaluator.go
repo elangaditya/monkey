d4bf5a684ba3455b9dc9bff16a99f232f945e560
Elang Aditya <elangadityar@gmail.com>
2025-03-25 13:49:09 +0700

    rearrange functions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -20,87 +20,6 @@ func isError(obj object.Object) bool {
 	return false
 }
 
-func Eval(node ast.Node, env *object.Environment) object.Object {
-	switch node := node.(type) {
-	case *ast.Program:
-		return evalProgram(node.Statements, env)
-
-	case *ast.LetStatement:
-		val := Eval(node.Value, env)
-		if isError(val) {
-			return val
-		}
-		env.Set(node.Name.Value, val)
-
-	case *ast.Identifier:
-		return evalIdentifier(node, env)
-
-	case *ast.ReturnStatement:
-		val := Eval(node.ReturnValue, env)
-		if isError(val) {
-			return val
-		}
-		return &object.ReturnValue{Value: val}
-
-	case *ast.ExpressionStatement:
-		return Eval(node.Expression, env)
-
-	case *ast.IntegerLiteral:
-		return &object.Integer{Value: node.Value}
-
-	case *ast.Boolean:
-		return nativeBoolToBooleanObject(node.Value)
-
-	case *ast.PrefixExpression:
-		right := Eval(node.Right, env)
-		if isError(right) {
-			return right
-		}
-		return evalPrefixExpression(node.Operator, right)
-
-	case *ast.InfixExpression:
-		left := Eval(node.Left, env)
-		if isError(left) {
-			return left
-		}
-
-		right := Eval(node.Right, env)
-		if isError(right) {
-			return right
-		}
-
-		return evalInfixExpression(node.Operator, left, right)
-
-	case *ast.BlockStatement:
-		return evalBlockStatement(node, env)
-
-	case *ast.IfExpression:
-		return evalIfExpression(node, env)
-
-	// Function keeps track of caller environment
-	// by assigning said environment to the struct
-	case *ast.FunctionLiteral:
-		params := node.Parameters
-		body := node.Body
-		return &object.Function{Parameters: params, Env: env, Body: body}
-
-	case *ast.CallExpression:
-		function := Eval(node.Function, env)
-		if isError(function) {
-			return function
-		}
-
-		args := evalExpressions(node.Arguments, env)
-		if len(args) == 1 && isError(args[0]) {
-			return args[0]
-		}
-
-		return applyFunction(function, args)
-	}
-
-	return nil
-}
-
 func evalProgram(stmts []ast.Statement, env *object.Environment) object.Object {
 	var res object.Object
 
@@ -118,26 +37,17 @@ func evalProgram(stmts []ast.Statement, env *object.Environment) object.Object {
 	return res
 }
 
-// Evaluation of program and block statements needs to be separated
-// because the object needs to be kept for the purposes flow control
-// and terminating parent evalProgram when encountering returnValue
-func evalBlockStatement(block *ast.BlockStatement, env *object.Environment) object.Object {
-	var res object.Object
+func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
+	val, ok := env.Get(node.Value)
 
-	for _, stmt := range block.Statements {
-		res = Eval(stmt, env)
-
-		if res != nil {
-			rt := res.Type()
-			if rt == object.RETURN_VALUE_OBJ || rt == object.ERROR_OBJ {
-				return res
-			}
-		}
+	if !ok {
+		return newError("identifier not found: " + node.Value)
 	}
 
-	return res
+	return val
 }
 
+// Convert native go boolean value to object.Boolean
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	if input {
 		return TRUE
@@ -219,6 +129,26 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 	}
 }
 
+// Evaluation of program and block statements needs to be separated
+// because the object needs to be kept for the purposes flow control
+// and terminating parent evalProgram when encountering returnValue
+func evalBlockStatement(block *ast.BlockStatement, env *object.Environment) object.Object {
+	var res object.Object
+
+	for _, stmt := range block.Statements {
+		res = Eval(stmt, env)
+
+		if res != nil {
+			rt := res.Type()
+			if rt == object.RETURN_VALUE_OBJ || rt == object.ERROR_OBJ {
+				return res
+			}
+		}
+	}
+
+	return res
+}
+
 func evalIfExpression(ie *ast.IfExpression, env *object.Environment) object.Object {
 	condition := Eval(ie.Condition, env)
 	if isError(condition) {
@@ -234,15 +164,6 @@ func evalIfExpression(ie *ast.IfExpression, env *object.Environment) object.Obje
 	}
 }
 
-func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
-	val, ok := env.Get(node.Value)
-
-	if !ok {
-		return newError("identifier not found: " + node.Value)
-	}
-
-	return val
-}
 
 func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
 	var result []object.Object
@@ -308,4 +229,85 @@ func isTruthy(obj object.Object) bool {
 
 func newError(format string, a ...interface{}) *object.Error {
 	return &object.Error{Message: fmt.Sprintf(format, a...)}
+}
+
+func Eval(node ast.Node, env *object.Environment) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalProgram(node.Statements, env)
+
+	case *ast.LetStatement:
+		val := Eval(node.Value, env)
+		if isError(val) {
+			return val
+		}
+		env.Set(node.Name.Value, val)
+
+	case *ast.Identifier:
+		return evalIdentifier(node, env)
+
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue, env)
+		if isError(val) {
+			return val
+		}
+		return &object.ReturnValue{Value: val}
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression, env)
+
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+
+	case *ast.PrefixExpression:
+		right := Eval(node.Right, env)
+		if isError(right) {
+			return right
+		}
+		return evalPrefixExpression(node.Operator, right)
+
+	case *ast.InfixExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+
+		right := Eval(node.Right, env)
+		if isError(right) {
+			return right
+		}
+
+		return evalInfixExpression(node.Operator, left, right)
+
+	case *ast.BlockStatement:
+		return evalBlockStatement(node, env)
+
+	case *ast.IfExpression:
+		return evalIfExpression(node, env)
+
+	// Function keeps track of caller environment
+	// by assigning said environment to the struct
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
+
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+
+		return applyFunction(function, args)
+	}
+
+	return nil
 }
